056f0f3aa265a2dab4bac5a343d59c626a02b882
konumaru <konumaru1022@gmail.com>
2024-07-11 17:14:19 +0900

    chore: Update BASE_PATH assignment in page.tsx and next.config.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import basePath from "../next.config.mjs";
 
-const BASE_PATH = basePath ? basePath : "" 
+const BASE_PATH = basePath ? basePath : "." 
 
 export default function Home() {
   return (
