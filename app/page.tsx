78936b642ec0d1cb1cf4e215e6b9d3c661e0e37a
konumaru <konumaru1022@gmail.com>
2024-07-12 11:51:50 +0900

    chore: Update page title to fix encoding issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl text-center font-bold mb-8 text-gray-800 ">konumaru's P5Gallery</h1>
+      <h1 className="text-4xl text-center font-bold mb-8 text-gray-800 ">konumaru&apos;s P5Gallery</h1>
       <ul className="space-y-4">
         <li>
           <Link href="/drawCircle" className="artwork-link text-2xl">drawCircle</Link>
