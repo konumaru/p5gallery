38dc83e627366d0c19c268b3e29e2158b6e406b6
konumaru <konumaru1022@gmail.com>
2024-07-18 10:45:14 +0900

    chore: Update favicon URL for production environment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 
 const nextConfig = {
     output: "export",
+    env: {
+        FAVICON_URL: process.env.NODE_ENV === 'production' ? '/favicon.svg' : '/next.svg',
+    },
 };
 
 export default nextConfig;
