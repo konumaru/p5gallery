739f257ce47dc6787ee6dc5cde32a0cf4f63929c
konumaru <konumaru1022@gmail.com>
2024-07-18 10:49:38 +0900

    chore: Update page to include favicon for production environment

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      <link rel="icon" href={process.env.FAVICON_URL} />
       <h1 className="text-4xl text-center font-bold mb-8 text-gray-800 ">konumaru&apos;s P5Gallery</h1>
       <ul className="space-y-4">
         <li>
